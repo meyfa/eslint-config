9b948d3ee4939606753838cf34d228a79ce6b801
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2023-07-17 11:28:07 +0200

    feat!: Update eslint-config-standard-with-typescript to v36

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,40 @@
 require('@rushstack/eslint-patch/modern-module-resolution.js')
 
 export = {
-  extends: 'standard-with-typescript',
+  extends: 'standard',
   rules: {
     // ES modules require a file extension on every import.
     // NPM packages should be exempted. For example, we want to allow extensionless imports from 'preact/hooks'.
     'import/extensions': ['error', 'always', { ignorePackages: true }]
   },
   overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: 'standard-with-typescript',
+      rules: {
+        // This is active in standard-with-typescript, but would be a huge migration task at this time.
+        '@typescript-eslint/consistent-type-imports': 'off',
+        // standard-with-typescript enforces no-confusing-void-expression even for the arrow shorthand.
+        // This makes writing React components basically impossible, so we need to override it here.
+        '@typescript-eslint/no-confusing-void-expression': [
+          'error',
+          {
+            ignoreArrowShorthand: true,
+            ignoreVoidOperator: false
+          }
+        ],
+        // Un-ban the {} type, which is the only concise way to express "any non-nullish value"
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            extendDefaults: true,
+            types: {
+              '{}': false
+            }
+          }
+        ]
+      }
+    },
     {
       files: 'test/**/*',
       rules: {
