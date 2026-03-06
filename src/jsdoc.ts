meyfa/eslint-config

commit 87a6cf99ee1e09aa9a0092b68f8fcd95b8cfc1c5

fix: Use defineConfig from eslint core (#293)

src/jsdoc.ts
@@ -1,8 +1,9 @@
+import { defineConfig } from 'eslint/config'
 import pluginJsdoc from 'eslint-plugin-jsdoc'
 
 const base = pluginJsdoc.configs['flat/recommended-typescript-error']
 
-export default {
+export default defineConfig({
   ...base,
 
   rules: {
@@ -54,4 +55,4 @@ export default {
       }
     ]
   }
-}
+})

src/eslint-plugin-import.d.ts
@@ -1,5 +1,5 @@
 declare module 'eslint-plugin-import' {
-  import { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.FlatConfig.Plugin
+  import { ESLint } from 'eslint'
+  const plugin: ESLint.Plugin
   export = plugin
 }

src/eslint-plugin-promise.d.ts
@@ -1,5 +1,5 @@
 declare module 'eslint-plugin-promise' {
-  import { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.FlatConfig.Plugin
+  import { ESLint } from 'eslint'
+  const plugin: ESLint.Plugin
   export = plugin
 }

src/index.ts
@@ -1,3 +1,5 @@
+import { defineConfig } from 'eslint/config'
+import { Linter } from 'eslint'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginImport from 'eslint-plugin-import'
@@ -14,11 +16,11 @@ const customizedStylistic = pluginStylistic.configs.customize({
   jsx: false
 })
 
-export default tseslint.config({
+export default defineConfig({
   extends: [
     eslint.configs.recommended,
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked
+    ...(tseslint.configs.strictTypeChecked as Linter.Config[]),
+    ...(tseslint.configs.stylisticTypeChecked as Linter.Config[])
   ],
 
   plugins: {

commit 03fbc487e0598dd30354c1999853a1dabb214084

fix: Restore documented JSDoc export behavior
The intended, and documented, usage of the JSDoc config is via spread
into a single `eslint.Config` object. However, this was broken since PR
#293 but not marked as a breaking change.

src/jsdoc.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'eslint/config'
+import type { Config } from 'eslint/config'
 import pluginJsdoc from 'eslint-plugin-jsdoc'
 
 const base = pluginJsdoc.configs['flat/recommended-typescript-error']
 
-export default defineConfig({
+export default {
   ...base,
 
   rules: {
@@ -55,4 +55,4 @@ export default defineConfig({
       }
     ]
   }
-})
+} satisfies Config

test/fixtures/eslint-test-config-jsdoc.js
@@ -6,8 +6,8 @@ import jsdocConfig from '../../src/jsdoc.ts'
 const rootDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..', '..')
 
 export default [
-  ...jsdocConfig,
   {
+    ...jsdocConfig,
     files: ['test/__generated__/**/*.{ts,tsx,js,jsx,mjs,cjs}'],
     languageOptions: {
       parser: tseslint.parser,